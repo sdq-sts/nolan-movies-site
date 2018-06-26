f864bfd30d500a64c4b9f588e07910002d610a95
Sadraque Santos <sdq.sts@gmail.com>
2018-06-26 03:03:13 +0000

    Tests for title and subtitle of TheHeader

--- test/specs/TheHeader.spec.js
+++ test/specs/TheHeader.spec.js
@@ -6,26 +6,27 @@ let wrapper
 
 const propsData = {
   title: ['The', 'Dream is', 'Real.'],
-  subtitle: ['The', 'Dream is', 'Real.']
+  subtitle: ['The', 'Dream is not', 'Real.']
 }
 
 test.beforeEach(() => {
   wrapper = mount(TheHeader, { propsData })
 })
 
-test('TheHeader.vue snapshot', (t) => {
-  t.snapshot({ html: wrapper.html() })
-})
+// test('TheHeader.vue snapshot', (t) => {
+//   t.snapshot({ html: wrapper.html() })
+// })
 
-test('TheHeader.vue to display .header .header__title', (t) => {
-  // const $items = wrapper.findAll('li').wrappers
-
-  // $items.forEach(($item, index) => {
-  //   t.is($item.text(), list[index])
-  // })
-
+test('TheHeader.vue to display the title', (t) => {
   const headerTitle = wrapper.findAll('.header .header__title').wrappers
  
   t.is(headerTitle.length, 1)
   t.is(headerTitle[0].text(), propsData.title.join(''))
+})
+
+test('TheHeader.vue to display the subtitle', (t) => {
+  const headerSubtitle = wrapper.findAll('.site-header .site-header__subtitle').wrappers
+
+  t.is(headerSubtitle.length, 1)
+  t.is(headerSubtitle[0].text(), propsData.subtitle.join(''))
 })
