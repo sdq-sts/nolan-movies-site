65fb0bbbb66f290447449bb28a6fb2d8f493c0f1
Sadraque Santos <sdq.sts@gmail.com>
2018-07-02 08:04:43 +0000

    Created PostsGrid component for Home page

---
+++ components/Home/PostsGrid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="posts-grid">
+    <PostsGridCard
+      class="posts-grid__card"
+      v-for="(post, i) in posts" :key="i"
+      :post="post"
+    />
+  </div>
+</template>
+
+<script>
+import PostsGridCard from './PostsGridCard'
+
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  },
+
+  components: {
+    PostsGridCard
+  }
+}
+</script>
+
+<style lang="scss">
+.posts-grid {
+  width: var(--site-width);
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 5%;
+
+  &__card {
+    width: 50%;
+    margin-bottom: 3em;
+
+    &:first-child {
+      margin-left: 50%;
+    }
+  }
+}
+</style>
---
+++ components/Home/PostsGridCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="grid-card">
+    <div class="grid-card__thumb">
+      <img :src="post.thumb" :alt="post.title">
+    </div>
+
+    <div class="grid-card__meta meta">
+      <h2 class="meta__title">{{ post.title }}</h2>
+      <span class="meta__year">{{ post.year }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.grid-card {
+  position: relative;
+
+  &__thumb {
+    width: 80%;
+    margin-left: auto;
+
+    img {
+      width: 100%;
+      display: block;
+    }
+  }
+
+  &__meta {
+    position: absolute;
+    bottom: 0;
+    left: 4em;
+  }
+}
+
+.meta {
+  &__title {
+    font-family: var(--main-font);
+    font-size: 2.5em;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: var(--text-color);
+  }
+
+  &__year {
+    font-family: var(--text-font);
+    display: block;
+    letter-spacing: 1px;
+    font-size: .65em;
+    padding-left: 3px;
+    color: var(--gray);
+  }
+}
+</style>
