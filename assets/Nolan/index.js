b36e5bdbc0467d774041feadc4fc306f4c4795ff
Sadraque Santos <sdq.sts@gmail.com>
2018-07-20 06:44:31 +0000

    Moved movie info to their own file

--- assets/Nolan/index.js
+++ assets/Nolan/index.js
@@ -1,3 +1,5 @@
+import amnesia from './movies/amnesia'
+
 export default {
   name: 'Christopher Nolan',
 
@@ -29,45 +31,12 @@ export default {
   },
 
   movies: [
-    {
-      title: 'Batman',
-      originalTitle: 'Batman',
-      year: 2008,
-      plot: 'Após um assalto que resultou na morte de sua esposa e que o deixou em estado gravíssimo, Leonard Shelby (Guy Pearce) passa a sofrer de amnésia recente. Ele não consegue se lembrar de fatos que aconteceram há 15 minutos. Mesmo assim ele decide ir atrás do assassino de sua mulher e se vingar.',
-      thumb: require('./images/inception/thumb.jpg'),
-      images: []
-    },
-    {
-      title: 'Batman',
-      originalTitle: 'Batman',
-      year: 2008,
-      plot: 'Após um assalto que resultou na morte de sua esposa e que o deixou em estado gravíssimo, Leonard Shelby (Guy Pearce) passa a sofrer de amnésia recente. Ele não consegue se lembrar de fatos que aconteceram há 15 minutos. Mesmo assim ele decide ir atrás do assassino de sua mulher e se vingar.',
-      thumb: require('./images/inception/thumb.jpg'),
-      images: []
-    },
-    {
-      title: 'Batman',
-      originalTitle: 'Batman',
-      year: 2008,
-      plot: 'Após um assalto que resultou na morte de sua esposa e que o deixou em estado gravíssimo, Leonard Shelby (Guy Pearce) passa a sofrer de amnésia recente. Ele não consegue se lembrar de fatos que aconteceram há 15 minutos. Mesmo assim ele decide ir atrás do assassino de sua mulher e se vingar.',
-      thumb: require('./images/inception/thumb.jpg'),
-      images: []
-    },
-    {
-      title: 'Batman',
-      originalTitle: 'Batman',
-      year: 2008,
-      plot: 'Após um assalto que resultou na morte de sua esposa e que o deixou em estado gravíssimo, Leonard Shelby (Guy Pearce) passa a sofrer de amnésia recente. Ele não consegue se lembrar de fatos que aconteceram há 15 minutos. Mesmo assim ele decide ir atrás do assassino de sua mulher e se vingar.',
-      thumb: require('./images/inception/thumb.jpg'),
-      images: []
-    },
-    {
-      title: 'Batman',
-      originalTitle: 'Batman',
-      year: 2008,
-      plot: 'Após um assalto que resultou na morte de sua esposa e que o deixou em estado gravíssimo, Leonard Shelby (Guy Pearce) passa a sofrer de amnésia recente. Ele não consegue se lembrar de fatos que aconteceram há 15 minutos. Mesmo assim ele decide ir atrás do assassino de sua mulher e se vingar.',
-      thumb: require('./images/inception/thumb.jpg'),
-      images: []
-    },
+    require('./movies/amnesia').default,
+    require('./movies/batman').default,
+    require('./movies/dunkirk').default,
+    require('./movies/inception').default,
+    require('./movies/memento').default
   ]
 }
+
+console.log(require('./movies/amnesia.js'))
