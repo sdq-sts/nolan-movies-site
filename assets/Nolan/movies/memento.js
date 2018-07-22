c98fd9eafbed600cdb4c9b8c39284093fd2bb8da
Sadraque Santos <sdq.sts@gmail.com>
2018-07-22 20:43:31 +0000

    Added big image to the post

--- assets/Nolan/movies/memento.js
+++ assets/Nolan/movies/memento.js
@@ -13,6 +13,7 @@ export default {
   plot: `Após um assalto que resultou na morte de sua esposa e que o deixou em estado gravíssimo, Leonard Shelby (Guy Pearce) passa a sofrer de amnésia recente. Ele não consegue se lembrar de fatos que aconteceram há 15 minutos. Mesmo assim ele decide ir atrás do assassino de sua mulher e se vingar.`,
   thumb: require('../images/memento/thumb.jpg'),
   images: {
-    postHeader: require('../images/memento/post-header.jpg')
+    postHeader: require('../images/memento/post-header.jpg'),
+    postBigImage: require('../images/memento/post-big-image.jpg')
   }
 }
--- pages/amnesia.vue
+++ pages/amnesia.vue
@@ -10,6 +10,14 @@
       />
 
       <PostCast class="post__cast" :castList="currentMovie.cast"/>
+
+      <div class="post__big-image big-image">
+        <img
+          class="big-image__img"
+          :src="currentMovie.images.postBigImage"
+          :alt="currentMovie.title"
+        />
+      </div>
     </article>
   </div>
 </template>
@@ -45,6 +53,14 @@ export default {
     width: var(--site-width);
     max-width: var(--site-max-width);
     margin: 0 auto;
+  }
+}
+
+.big-image {
+  margin-top: 4em;
+
+  &__img {
+    width: 100%;
   }
 }
 </style>
