f5d5d73562e6690e639ab6f8e1f7c4fa1d358efe
Sadraque Santos <sdq.sts@gmail.com>
2018-07-24 02:42:11 +0000

    Created Post component

---
+++ components/Pages/Post.vue
@@ -0,0 +1,85 @@
+<template>
+  <article class="post">
+    <PostHeader
+      class="post__header"
+      :title="currentMovie.title"
+      :originalTitle="currentMovie.originalTitle"
+      :year="currentMovie.year"
+      :image="currentMovie.images.postHeader"
+    />
+
+    <PostCast
+      class="post__cast"
+      :castList="currentMovie.cast"
+    />
+
+    <div class="post__big-image big-image">
+      <img
+        class="big-image__img"
+        :src="currentMovie.images.postBigImage"
+        :alt="currentMovie.title"
+      />
+    </div>
+
+    <PostAbout
+      class="post__about-movie"
+      :text="currentMovie.about"
+      :firstImage="currentMovie.images.firstAboutImage"
+      :secondImage="currentMovie.images.secondAboutImage"
+    />
+
+    <PostPlot
+      class="post__plot"
+      :plot="currentMovie.plot"
+      :smallImageSrc="currentMovie.images.plotSmallImg"
+      :bigImageSrc="currentMovie.images.plotBigImg"
+      :imdbLink="currentMovie.imdbLink"
+    />
+  </article>
+</template>
+
+<script>
+import PostHeader from '@/components/Pages/PostHeader'
+import PostCast from '@/components/Pages/PostCast'
+import PostAbout from '@/components/Pages/PostAbout'
+import PostPlot from '@/components/Pages/PostPlot'
+
+export default {
+  computed: {
+    movieIndex () {
+      return this.$store.getters.currentMovie !== null ? this.$store.getters.currentMovie : 0
+    },
+
+    currentMovie () {
+      return this.$store.getters.getMovie(this.movieIndex)
+    }
+  },
+
+  components: { PostHeader, PostCast, PostAbout, PostPlot }
+}
+</script>
+
+<style lang="scss">
+.post {
+  &__header,
+  &__cast,
+  &__about-movie,
+  &__plot {
+    width: var(--site-width);
+    max-width: var(--site-max-width);
+    margin: 0 auto;
+  }
+
+  &__about-movie,
+  &__big-image,
+  &__plot {
+    margin-top: var(--gutter);
+  }
+}
+
+.big-image {
+  &__img {
+    width: 100%;
+  }
+}
+</style>
